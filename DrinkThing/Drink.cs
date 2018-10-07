84157047a017a4fd8514ed4101a159476d3ca61f
Xris Karaplis <xriskaraplis@Xriss-MacBook-Pro.local>
2018-10-06 20:45:23 -0400

    first commit

---
+++ DrinkThing/Drink.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DrinkThing
+{
+    public class Drink
+    {
+        // Build the list of ingredients from strIngredient + strMeasure properties returned from the API --
+        // if an ingredient is not returned as an empty string, add it to the list, along with its corresponding measurement
+        public List<string> Ingredients
+        {
+            get
+            {
+                List<string> ingredients = new List<string>();
+
+                if (strIngredient1 != "")
+                {
+                    string ingred1 = "";
+
+                    if (strMeasure1 != "\n")
+                    {
+                        ingred1 = strMeasure1 + " " + strIngredient1;
+                    }
+                    else
+                    {
+                        ingred1 = strIngredient1;
+                    }
+                    ingredients.Add(ingred1);
+                }
+                if (strIngredient2 != "")
+                {
+                    string ingred2 = "";
+
+                    if (strMeasure2 != "\n")
+                    {
+                        ingred2 = strMeasure2 + " " + strIngredient2;
+                    }
+                    else
+                    {
+                        ingred2 = strIngredient2;
+                    }
+                    ingredients.Add(ingred2);
+                }
+                if (strIngredient3 != "")
+                {
+                    string ingred3 = "";
+
+                    if (strMeasure3 != "\n")
+                    {
+                        ingred3 = strMeasure3 + " " + strIngredient3;
+                    }
+                    else
+                    {
+                        ingred3 = strIngredient3;
+                    }
+                    ingredients.Add(ingred3);
+                }
+                if (strIngredient4 != "")
+                {
+                    string ingred4 = "";
+
+                    if (strMeasure4 != "\n")
+                    {
+                        ingred4 = strMeasure4 + " " + strIngredient4;
+                    }
+                    else
+                    {
+                        ingred4 = strIngredient4;
+                    }
+                    ingredients.Add(ingred4);
+                }
+                if (strIngredient5 != "")
+                {
+                    string ingred5 = "";
+
+                    if (strMeasure5 != "\n")
+                    {
+                        ingred5 = strMeasure5 + " " + strIngredient5;
+                    }
+                    else
+                    {
+                        ingred5 = strIngredient5;
+                    }
+                    ingredients.Add(ingred5);
+                }
+                if (strIngredient6 != "")
+                {
+                    string ingred6 = "";
+
+                    if (strMeasure6 != "\n")
+                    {
+                        ingred6 = strMeasure6 + " " + strIngredient6;
+                    }
+                    else
+                    {
+                        ingred6 = strIngredient6;
+                    }
+                    ingredients.Add(ingred6);
+                }
+                if (strIngredient7 != "")
+                {
+                    string ingred7 = "";
+
+                    if (strMeasure7 != "\n")
+                    {
+                        ingred7 = strMeasure7 + " " + strIngredient7;
+                    }
+                    else
+                    {
+                        ingred7 = strIngredient7;
+                    }
+                    ingredients.Add(ingred7);
+                }
+                if (strIngredient8 != "")
+                {
+                    string ingred8 = "";
+
+                    if (strMeasure8 != "\n")
+                    {
+                        ingred8 = strMeasure8 + " " + strIngredient8;
+                    }
+                    else
+                    {
+                        ingred8 = strIngredient8;
+                    }
+                    ingredients.Add(ingred8);
+                }
+                if (strIngredient9 != "")
+                {
+                    string ingred9 = "";
+
+                    if (strMeasure9 != "\n")
+                    {
+                        ingred9 = strMeasure9 + " " + strIngredient9;
+                    }
+                    else
+                    {
+                        ingred9 = strIngredient9;
+                    }
+                    ingredients.Add(ingred9);
+                }
+                if (strIngredient10 != "")
+                {
+                    string ingred10 = "";
+
+                    if (strMeasure10 != "\n")
+                    {
+                        ingred10 = strMeasure10 + " " + strIngredient10;
+                    }
+                    else
+                    {
+                        ingred10 = strIngredient10;
+                    }
+                    ingredients.Add(ingred10);
+                }
+                if (strIngredient11 != "")
+                {
+                    string ingred11 = "";
+
+                    if (strMeasure11 != "\n")
+                    {
+                        ingred11 = strMeasure11 + " " + strIngredient11;
+                    }
+                    else
+                    {
+                        ingred11 = strIngredient11;
+                    }
+                    ingredients.Add(ingred11);
+                }
+                if (strIngredient12 != "")
+                {
+                    string ingred12 = "";
+
+                    if (strMeasure12 != "\n")
+                    {
+                        ingred12 = strMeasure12 + " " + strIngredient12;
+                    }
+                    else
+                    {
+                        ingred12 = strIngredient12;
+                    }
+                    ingredients.Add(ingred12);
+                }
+                if (strIngredient13 != "")
+                {
+                    string ingred13 = "";
+
+                    if (strMeasure13 != "\n")
+                    {
+                        ingred13 = strMeasure13 + " " + strIngredient13;
+                    }
+                    else
+                    {
+                        ingred13 = strIngredient13;
+                    }
+                    ingredients.Add(ingred13);
+                }
+                if (strIngredient14 != "")
+                {
+                    string ingred14 = "";
+
+                    if (strMeasure14 != "\n")
+                    {
+                        ingred14 = strMeasure14 + " " + strIngredient14;
+                    }
+                    else
+                    {
+                        ingred14 = strIngredient14;
+                    }
+                    ingredients.Add(ingred14);
+                }
+                if (strIngredient15 != "")
+                {
+                    string ingred15 = "";
+
+                    if (strMeasure15 != "\n")
+                    {
+                        ingred15 = strMeasure15 + " " + strIngredient15;
+                    }
+                    else
+                    {
+                        ingred15 = strIngredient15;
+                    }
+                    ingredients.Add(ingred15);
+                }
+
+                return ingredients;
+            }
+        }
+
+        public string idDrink { get; set; }
+        public string strDrink { get; set; }
+        public string strCategory { get; set; }
+        public string strIBA { get; set; }
+        public string strAlcoholic { get; set; }
+        public string strGlass { get; set; }
+        public string strInstructions { get; set; }
+        public string strDrinkThumb { get; set; }
+        public string strIngredient1 { get; set; }
+        public string strIngredient2 { get; set; }
+        public string strIngredient3 { get; set; }
+        public string strIngredient4 { get; set; }
+        public string strIngredient5 { get; set; }
+        public string strIngredient6 { get; set; }
+        public string strIngredient7 { get; set; }
+        public string strIngredient8 { get; set; }
+        public string strIngredient9 { get; set; }
+        public string strIngredient10 { get; set; }
+        public string strIngredient11 { get; set; }
+        public string strIngredient12 { get; set; }
+        public string strIngredient13 { get; set; }
+        public string strIngredient14 { get; set; }
+        public string strIngredient15 { get; set; }
+        public string strMeasure1 { get; set; }
+        public string strMeasure2 { get; set; }
+        public string strMeasure3 { get; set; }
+        public string strMeasure4 { get; set; }
+        public string strMeasure5 { get; set; }
+        public string strMeasure6 { get; set; }
+        public string strMeasure7 { get; set; }
+        public string strMeasure8 { get; set; }
+        public string strMeasure9 { get; set; }
+        public string strMeasure10 { get; set; }
+        public string strMeasure11 { get; set; }
+        public string strMeasure12 { get; set; }
+        public string strMeasure13 { get; set; }
+        public string strMeasure14 { get; set; }
+        public string strMeasure15 { get; set; }
+        public string dateModified { get; set; }
+    }
+}
+
---
+++ DrinkThing/Program.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DrinkThing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize a string for user choice, set as an empty string
+            string choice = "";
+
+            List<Drink> drinks = new List<Drink>();
+
+            // While the user's choice is not "Q" (or "q," which will be capitalized on input),
+            // continue this loop
+            while (choice != "Q")
+            {
+
+                // Get a user's choice
+                choice = PromptUserChoice();
+
+                // Check user's choice against switch cases
+                switch (choice)
+                {
+                    // If "1" is entered,
+                    case "1":
+                        // search drink by name,
+                        string drinkToFind = SearchByName();
+                        // provide a list of drinks that match that name,
+                        drinks = GetDrinksByDrinkName(drinkToFind);
+                        // and return the ingredients and instructions for a selected drink
+                        if(drinks == null || drinks.Count == 0) {
+                            break;
+                        }
+                        ReturnIngredientsAndInstructions(drinks);
+                        break;
+                    // If "2" is entered,
+                    case "2":
+                        string ingredientToSearchBy = SearchByIngredient();
+                        drinks = DrinksByIngredient(ingredientToSearchBy);
+                        if(drinks == null || drinks.Count == 0) {
+                            break;
+                        }
+                        ReturnIngredientsAndInstructions(drinks);
+                        break;
+                    case "3":
+                        drinks = GetRandomDrink();
+                        ReturnIngredientsAndInstructions(drinks);
+                        break;
+                    default:
+                        Console.Clear();
+                        break;
+                }
+
+                // Prompt for another selection, or allow user to exit program
+                Console.WriteLine("\nWould you like to search again, or (Q)uit?");
+
+                // Get an updated choice
+                choice = Console.ReadLine();
+                // If a letter is entered, ensure it is capitalized
+                choice = CapitalizeLetters(choice);
+
+                // Wait 1 second (Thread.Sleep() takes millisecond increments)
+                Thread.Sleep(1000);
+
+                // Clear the console
+                Console.Clear();
+            }
+
+            // Say farewell to your user
+            Console.WriteLine("Goodbye!");
+
+        }
+
+        /// <summary>
+        /// Checks an entered string, returns it uppercased if it is composed of ONLY letters
+        /// </summary>
+        /// <returns>A string, capitalized if all its characters are alphabetical</returns>
+        /// <param name="choice">The user's choice</param>
+        static string CapitalizeLetters(string choice)
+        {
+
+            if (Regex.IsMatch(choice, @"^[a-zA-Z]+$"))
+            {
+                choice = choice.ToUpper();
+            }
+
+            return choice;
+        }
+
+        /// <summary>
+        /// Prompts a user for its choice
+        /// </summary>
+        /// <returns>The user's choice</returns>
+        static string PromptUserChoice()
+        {
+
+            Console.WriteLine("Welcome human. How would you like to search for your adult beverage*?");
+            Console.WriteLine("*(NOTE: adult beverage means a beverage intended for adult consumption,\n not a beverage that is itself of age.)");
+
+            Console.WriteLine("1. Search by drink name");
+            Console.WriteLine("2. Search by ingredient");
+            Console.WriteLine("3. YOLO -- Give me a random drink");
+            Console.WriteLine("Q. Make it stop");
+
+            string choice = Console.ReadLine();
+
+            // Capitalize letters (if all characters in entered string are alphabetical)
+            choice = CapitalizeLetters(choice);
+
+            // Return the provided choice
+            return choice;
+        }
+
+        /// <summary>
+        /// Gets name of a drink to search for
+        /// </summary>
+        /// <returns>The given name.</returns>
+        static string SearchByName()
+        {
+            // Get user input to search for
+            Console.Write("What drink would you like to search for?: ");
+            string drinkToFind = Console.ReadLine();
+            return drinkToFind;
+        }
+
+        static string SearchByIngredient()
+        {
+            Console.Write("What ingredient would you like to search by?: ");
+            string ingredientToFind = Console.ReadLine();
+            return ingredientToFind;
+        }
+
+        /// <summary>
+        /// Gets a list of drinks by a given ingredient
+        /// </summary>
+        /// <returns>A list of drinks</returns>
+        /// <param name="ingredient">The ingredient to search by</param>
+        static List<Drink> DrinksByIngredient(string ingredient)
+        {
+            string url = $"https://www.thecocktaildb.com/api/json/v1/1/filter.php?i={ingredient}";
+
+            List<Drink> drinks = GetDrinksFromAPI(url);
+
+            for (int i = 0; i < drinks.Count; i++)
+            {
+                drinks[i] = GetDrinkDetails(drinks[i]);
+            }
+
+            return drinks;
+        }
+
+        /// <summary>
+        /// Gets drinks from provided URL
+        /// </summary>
+        /// <returns>The drinks from API</returns>
+        /// <param name="url">URL for API</param>
+        static List<Drink> GetDrinksFromAPI(string url)
+        {
+            // Get the response from the URL as a string
+            string json = new WebClient().DownloadString(url);
+
+            List<Drink> drinks = new List<Drink>();
+
+            // If there is no match for the search,
+            if (string.IsNullOrEmpty(json))
+            {
+                // notify user
+                NothingFound();
+                return drinks;
+            }
+
+            // Get the amount of the string to chop off to keep only the array of objects that is returned
+            int lengthToChop = json.Length - 11;
+
+            // Replace the current json body with the body minus the string "drinks," using lengthToChop previously declared
+            json = (json.Substring(10, lengthToChop));
+
+            // Get a list of drinks (as returned from the API), deserialized as a list of Drink objects
+            drinks = JsonConvert.DeserializeObject<List<Drink>>(json);
+
+            if(drinks == null) {
+                // notify user
+                NothingFound();
+                return drinks;
+            }
+
+            if (drinks.Count > 1)
+            {
+                ListDrinks(drinks);
+            }
+
+            return drinks;
+        }
+
+        /// <summary>
+        /// List drinks
+        /// </summary>
+        /// <param name="drinks">List of drinks to list</param>
+        static void ListDrinks(List<Drink> drinks)
+        {
+            // Return drinks list by...
+            Console.WriteLine("Your drinks!!!:");
+            // ... looping through each drink result 
+            for (int i = 0; i < drinks.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.) {drinks[i].strDrink}");
+            }
+
+            return;
+
+        }
+
+        /// <summary>
+        /// Gets a selection of drinks from API
+        /// </summary>
+        /// <returns>A list of drinks from API response</returns>
+        /// <param name="drinkToFind">The drink name to search for</param>
+        static List<Drink> GetDrinksByDrinkName(string drinkToFind)
+        {
+            // Append user-chosen drink to URL string
+            string url = $"https://www.thecocktaildb.com/api/json/v1/1/search.php?s={drinkToFind}";
+
+            // Return the list that was found
+            return GetDrinksFromAPI(url);
+        }
+
+        /// <summary>
+        /// Gets a random drink
+        /// </summary>
+        /// <returns>A random drink</returns>
+        static List<Drink> GetRandomDrink()
+        {
+            string url = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+
+            return GetDrinksFromAPI(url);
+        }
+
+        /// <summary>
+        /// Returns ingredients and instructions for a user's drink selection
+        /// </summary>
+        /// <param name="drinks">A list of drinks to select from</param>
+        static void ReturnIngredientsAndInstructions(List<Drink> drinks)
+        {
+
+            if (drinks.Count > 1)
+            {
+                PromptForDrinkFromList(drinks);
+            }
+            else
+            {
+                // Return ingredients
+                Console.WriteLine($"\nFor a {drinks[0].strDrink}, you'll need...");
+                // Loop through each ingredient for the drink, and write each one
+                foreach (string ingredient in drinks[0].Ingredients)
+                {
+                    Console.WriteLine(ingredient);
+                }
+
+                // Return recipe for drink
+                Console.WriteLine("\nDo this:");
+                // List instructions for the given drink
+                Console.WriteLine(drinks[0].strInstructions);
+            }
+
+            return;
+        }
+
+        /// <summary>
+        /// Prompts a user for a drink from a list of drinks
+        /// </summary>
+        /// <param name="drinks">A list of drinks</param>
+        static void PromptForDrinkFromList(List<Drink> drinks)
+        {
+            // Prompt for a choice from the list
+            Console.WriteLine("\nYour choice for a recipe?");
+
+            // Get the choice (-1 to compensate for 0-based index --
+            // see where the index is i+1 in the loop above)
+            int index = int.Parse(Console.ReadLine()) - 1;
+
+            // Return ingredients
+            Console.WriteLine($"\nFor a {drinks[index].strDrink}, you'll need...");
+            // Loop through each ingredient for the drink, and write each one
+            foreach (string ingredient in drinks[index].Ingredients)
+            {
+                Console.WriteLine(ingredient);
+            }
+
+            // Return recipe for drink
+            Console.WriteLine("\nDo this:");
+            // List instructions for the given drink
+            Console.WriteLine(drinks[index].strInstructions);
+
+            return;
+        }
+
+        /// <summary>
+        /// Gets details for a given drink
+        /// </summary>
+        /// <returns>The drink, with details filled in</returns>
+        /// <param name="drink">A drink to search for</param>
+        static Drink GetDrinkDetails(Drink drink)
+        {
+            // Append user-chosen drink ID to URL
+            string url = $"https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i={drink.idDrink}";
+
+            // Get the response from the URL as a string
+            string json = new WebClient().DownloadString(url);
+
+            // Get the amount of the string to chop off to keep only the array of objects that is returned
+            int lengthToChop = json.Length - 11;
+
+            // Replace the current json body with the body minus the string "drinks," using lengthToChop previously declared
+            json = (json.Substring(10, lengthToChop));
+
+            // Get a list of drinks (as returned from the API), deserialized as a list of Drink objects
+            List<Drink> drinks = JsonConvert.DeserializeObject<List<Drink>>(json);
+
+            return drinks[0];
+
+        }
+
+        /// <summary>
+        /// If no matches for a search is found, return on down to the program backbone
+        /// </summary>
+        static void NothingFound()
+        {
+            Console.WriteLine("Nothing found for that search. Press any key to try again.");
+            string answer = Console.ReadLine();
+
+            answer = CapitalizeLetters(answer);
+
+            Console.Clear();
+            return;
+        }
+
+    }
+}
